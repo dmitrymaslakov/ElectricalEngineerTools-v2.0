2eb7156e14a967a4dfa364c12cb623541daf9d48
Dzmitry Maslakou <dmitrymaslakov@yandex.ru>
2022-07-24 20:35:18 +0300

    Add external acad.js file

--- UserInterface/src/components/Management.jsx
+++ UserInterface/src/components/Management.jsx
@@ -2,13 +2,16 @@ import React from 'react'
 import ButtonGroup from 'react-bootstrap/ButtonGroup'
 import Button from 'react-bootstrap/Button'
 
-const Management = () => {
-    return <ButtonGroup vertical className='my-3'>
-        <Button variant='secondary'>Расчет освещенности</Button>
-        <Button variant='secondary'>Расставить светильники</Button>        
-        <Button variant='secondary'>Вставить светильник</Button>        
-        <Button variant='secondary'>Менеджмент светильников</Button>        
-    </ButtonGroup>
+class Management extends React.Component {
+
+    render() {
+        return <ButtonGroup vertical className='my-3'>
+            <Button onClick={window['InvokeRead']} variant='secondary'>Расчет освещенности</Button>
+            <Button onClick={window['InvokeWrite']} variant='secondary'>Расставить светильники</Button>
+            <Button variant='secondary'>Вставить светильник</Button>
+            <Button variant='secondary'>Менеджмент светильников</Button>
+        </ButtonGroup>
+    }
 }
 
 export default Management
