1fd5c97033eaea339ddd48abd99b2606ec5f2de7
Dzmitry <dmitrymaslakov@yandex.ru>
2023-05-01 19:25:36 +0300

    router

--- UserInterface/src/App.js
+++ UserInterface/src/App.js
@@ -1,59 +1,13 @@
-import React, { useEffect, useState } from 'react'
-import MyModal from './components/UI/MyModal/MyModal'
-
-import PostList from './components/PostList'
-import PostForm from './components/PostForm'
-import PostFilter from './components/PostFilter'
-import './styles/App.css'
-import MyButton from './components/UI/button/MyButton'
-import { usePosts } from './components/hooks/usePosts'
-import PostService from './api/PostService'
-import Loader from './components/UI/Loader/Loader'
-import { useFetching } from './components/hooks/useFetching'
+import React from 'react'
+import { Link } from 'react-router-dom'
 
 const App = () => {
-  const [posts, setPosts] = useState([])
-  const [filter, setFilter] = useState({ sort: '', query: '' })
-  const [modal, setModal] = useState(false)
-  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
-  const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
-    const posts = await PostService.getAll()
-    setPosts(posts)
-  })
-
-  useEffect(() => {
-    fetchPosts()
-  }, [])
-
-  const createPost = (newPost) => {
-    setPosts([...posts, newPost])
-    setModal(false)
-  }
-
-  const removePost = (post) => {
-    setPosts(posts.filter(p => p.id !== post.id))
-  }
-
   return (
-    <div className='App'>
-      <MyButton style={{ marginTop: 30 }} onClick={() => setModal(true)}>
-        Создать пользователя
-      </MyButton>
-      <MyModal visible={modal} setVisible={setModal}>
-        <PostForm create={createPost} />
-      </MyModal>
-      <hr style={{ margin: '15px 0' }} />
-      <PostFilter
-        filter={filter}
-        setFilter={setFilter} />
-      {postError &&
-        <h1 style={{ textAlign: 'center' }}>Произошла ошибка ${postError}</h1>
-
-      }
-      {isPostsLoading
-        ? <div style={{ display: 'flex', justifyContent: 'center', marginTop: 50 }}><Loader /></div>
-        : <PostList remove={removePost} posts={sortedAndSearchedPosts} title='Посты для JS' />
-      }
+    <div className='navbar'>
+      <div className='navbar__links'>
+        <Link to='/about'>О сайте</Link>
+        <Link to='/posts'>Посты</Link>
+      </div>
     </div>
   )
 }
--- UserInterface/src/index.js
+++ UserInterface/src/index.js
@@ -1,8 +1,35 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import {
+    createBrowserRouter,
+    createRoutesFromElements,
+    Route,
+    RouterProvider
+} from 'react-router-dom'
+import About from './pages/About'
+import Posts from './pages/Posts'
+
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+        children: [
+            {
+                path: "/about",
+                element: <About />,
+            },
+        ]
+    },
+    
+    {
+        path: "/posts",
+        element: <Posts />,
+    }
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-      <App />
+    <RouterProvider router={router} />
 )
